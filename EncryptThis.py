eb7e564c515e68b07ea2b6c5c8169ec692d3a624
Agata <108606980+modjeskaa@users.noreply.github.com>
2023-02-09 11:13:06 +0100

    Update EncryptThis.py

--- EncryptThis.py
+++ EncryptThis.py
@@ -1,3 +1,12 @@
+#   https://www.codewars.com/kata/5848565e273af816fb000449
+#   You want to create secret messages which can be deciphered by the Decipher this! kata. 
+#   Here are the conditions:
+#   1. Your message is a string containing space separated words.
+#   2. You need to encrypt each word in the message using the following rules:
+#       - The first letter must be converted to its ASCII code.
+#       - The second letter must be switched with the last letter
+#   Keepin' it simple: There are no special characters in the input.
+
 def encrypt_this(text):
     text_to_list = text.split(" ")
     list = []
@@ -15,5 +24,3 @@ def encrypt_this(text):
             full_text = f"{first_letter}{word[-1]}{word[2:-1]}{replacement}"
             list.append(full_text)
     return " ".join(list)
-
-print(encrypt_this("A wise old owl lived in an oak"))

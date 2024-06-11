69cba0c3bba331a0db654a99ac83cb233edd5688
Agata <108606980+modjeskaa@users.noreply.github.com>
2024-06-11 17:47:24 +0200

    Update BitCounting.py

--- BitCounting.py
+++ BitCounting.py
@@ -2,6 +2,13 @@
 #   Write a function that takes an integer as input, and returns the number of bits 
 #   that are equal to one in the binary representation of that number. 
 
-    def count_bits(n):
-        binary_representation = (bin(n)[2:])
-        return binary_representation.count("1")
+def count_bits(n):
+    binary_representation = (bin(n)[2:])
+    return binary_representation.count("1")
+
+def main():
+    number = 11
+    print(f"Number of bits that are equal to 1 in the binary representation of {number} is {count_bits(number)}.")
+
+if __name__ == "__main__":
+    main()
